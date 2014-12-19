16ac30aeacbf6b5b667c117f36149a9029f58642
Angela Hsu <ahsu@cs.utexas.edu>
2014-12-18 21:09:00 -0600

    Added recent entries column; coverage tests

--- blog/tests.py
+++ blog/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.template import Template, Context
 
 from .models import Entry
 
@@ -62,3 +63,27 @@ class EntryViewTest(TestCase):
     def test_body_in_entry(self):
         response = self.client.get(self.entry.get_absolute_url())
         self.assertContains(response, self.entry.body)
+
+class EntryHistoryTagTest(TestCase):
+
+    TEMPLATE = Template("{% load blog_tags %} {% entry_history %}")
+
+    def setUp(self):
+        self.user = get_user_model().objects.create(username="some user")
+
+    def test_entry_shows_up(self):
+        entry = Entry.objects.create(author=self.user, title="title")
+        rendered = self.TEMPLATE.render(Context({}))
+        self.assertIn(entry.title, rendered)
+
+    def test_no_posts(self):
+        rendered = self.TEMPLATE.render(Context({}))
+        self.assertIn("No recent entries.", rendered)
+
+    def test_many_posts(self):
+        for n in range(6):
+            Entry.objects.create(author=self.user, title="Post #{0}".format(n))
+        rendered = self.TEMPLATE.render(Context({}))
+        self.assertIn("Post #5", rendered)
+        self.assertNotIn("Post #6", rendered)
+
