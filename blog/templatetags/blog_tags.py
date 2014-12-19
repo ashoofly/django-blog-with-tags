30e06e488fe74a5fe47eb97dcfa13b222fbc8e1a
Angela Hsu <ahsu@cs.utexas.edu>
2014-12-19 15:58:14 -0600

    adding tag list view

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,10 +1,16 @@
 from django import template
+from taggit.models import Tag
+
 from ..models import Entry
 
 register = template.Library()
 
 @register.inclusion_tag('blog/_entry_history.html')
 def entry_history():
-    entries = Entry.objects.all()[:6]
+    entries = Entry.objects.order_by('-created_at')[:6]
     return {'entries': entries}
 
+@register.inclusion_tag('blog/_tag_list.html')
+def tag_list():
+    tags = Tag.objects.order_by('name')
+    return {'tags': tags}
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,23 @@
 from django.views.generic import DetailView, ListView
+from taggit.models import Tag
+
 from .models import Entry
+
+class TagMixin(object):
+    def get_context_data(self, **kwargs):
+        context = super(TagMixin, self).get_context_data(**kwargs)
+        context['tags'] = Tag.objects.all()
+        return context
+
 
 class EntryDetail(DetailView):
     model = Entry
 
-class EntryIndex(ListView):
+class EntryIndex(TagMixin, ListView):
     template_name = 'index.html'
     queryset = Entry.objects.order_by('-created_at')
 
-class TagIndexView(ListView):
+class TagIndexView(TagMixin, ListView):
     template_name = 'index.html'
     model = Entry
     
